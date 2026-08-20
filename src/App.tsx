eaaef1720095424610da351a6feffa4d95d15d96
dennybadmam <dennygodzilla0@gmail.com>
2026-08-20 12:24:12 +0100

    feat: routes for landing, auth, dashboard

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import { LoginPage } from "./pages/LoginPage";
+import { DashboardLayout } from "./layouts/DashboardLayout";
+import { OverviewPage } from "./pages/OverviewPage";
+import { NumbersPage } from "./pages/NumbersPage";
+import { WalletPage } from "./pages/WalletPage";
+import { InboxPage } from "./pages/InboxPage";
+import { AuthProvider, useAuth } from "./lib/auth";
+
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" replace />;
+  return children;
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/app"
+          element={
+            <PrivateRoute>
+              <DashboardLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<OverviewPage />} />
+          <Route path="numbers" element={<NumbersPage />} />
+          <Route path="inbox" element={<InboxPage />} />
+          <Route path="wallet" element={<WalletPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AuthProvider>
+  );
+}
