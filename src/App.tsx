de5436c06331ab04fe5d885509320920d3ab8a54
dennybadmam <dennygodzilla0@gmail.com>
2026-08-20 18:26:46 +0100

    feat: add register route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 import { DashboardLayout } from "./layouts/DashboardLayout";
 import { OverviewPage } from "./pages/OverviewPage";
 import { NumbersPage } from "./pages/NumbersPage";
@@ -20,6 +21,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route
           path="/app"
           element={
